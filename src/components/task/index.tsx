944b049694c4942cae3536633153859f0fb8abc1
thiago <desenvolvedor.nunes@gmail.com>
2023-05-22 20:17:01 -0300

    feat: cria parametros para mudar a quantidade de tasks criadas e concluidas

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Image, Touchable, TouchableOpacity } from 'react-native'
+import { Image, TouchableOpacity } from 'react-native'
 
 import CheckdIcon from '@assets/checkedIcon.png'
 import { Button } from '@components/button'
@@ -10,14 +10,31 @@ import * as Styled from './styled'
 export type TaskProps = {
   taskText: string
   removeTask: () => void
+  completedTasks: number
+  setCompletedTasks: React.Dispatch<React.SetStateAction<number>>
 }
 
-export const Task = ({ taskText = '', removeTask }: TaskProps): JSX.Element => {
+export const Task = ({
+  taskText = '',
+  removeTask,
+  completedTasks,
+  setCompletedTasks
+}: TaskProps): JSX.Element => {
   const [isCompleted, setIsCompleted] = useState(false)
 
   return (
     <Styled.Container>
-      <TouchableOpacity onPress={() => setIsCompleted(!isCompleted)}>
+      <TouchableOpacity
+        onPress={() => {
+          setIsCompleted(!isCompleted)
+
+          if (isCompleted) {
+            return setCompletedTasks(completedTasks - 1)
+          } else {
+            return setCompletedTasks(completedTasks + 1)
+          }
+        }}
+      >
         <Image source={isCompleted ? CheckdIcon : UnCheckedIcon} />
       </TouchableOpacity>
       <Styled.TaskText isCompleted={isCompleted}>{taskText}</Styled.TaskText>
