90b4a16d95702b557257fecb6cc0b9dbb0eabb08
thiago <desenvolvedor.nunes@gmail.com>
2023-05-22 20:03:26 -0300

    style: cria estilo do componente task

---
+++ src/components/task/styled.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components/native'
+
+type TaskTextProps = {
+  isCompleted?: boolean
+}
+
+export const Container = styled.View`
+  height: 64px;
+  width: 341px;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+
+  border-radius: 6px;
+  margin-bottom: 8px;
+
+  background: ${({ theme }) => theme.colors.gray[500]};
+`
+
+export const TaskText = styled.Text<TaskTextProps>`
+  width: 235px;
+  margin-left: 12px;
+  color: ${({ theme, isCompleted }) =>
+    isCompleted ? theme.colors.gray[300] : theme.colors.gray[100]};
+  text-decoration: ${({ isCompleted }) => isCompleted && 'line-through'};
+
+  font-size: ${({ theme }) => theme.font_size.MD}px;
+  font-family: ${({ theme }) => theme.font_familly.regular};
+`
