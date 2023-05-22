5d1561aa6d4bb86f8ea8d2873e8fb74399672724
thiago <desenvolvedor.nunes@gmail.com>
2023-05-22 19:56:42 -0300

    chore: adiciona tema, tipagens do tema e tipagens de imagens

---
+++ src/theme/index.tsx
@@ -0,0 +1,33 @@
+export const theme = {
+  colors: {
+    gray: {
+      100: '#F2F2F2',
+      200: '#D9D9D9',
+      300: '#808080',
+      400: '#333333',
+      500: '#262626',
+      600: '#1A1A1A',
+      700: '#0D0D0D'
+    },
+    blue: {
+      100: '#4EA8DE',
+      900: '#1E6F9F'
+    },
+    perple: {
+      100: '#8284FA',
+      900: '#5E60CE'
+    },
+    red: {
+      100: '#E25858'
+    }
+  },
+  font_familly: {
+    regular: 'Inter_400Regular',
+    bold: 'Inter_700Bold'
+  },
+  font_size: {
+    SM: 12,
+    MD: 14,
+    LG: 16
+  }
+}
